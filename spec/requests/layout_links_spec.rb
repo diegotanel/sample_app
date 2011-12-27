3087f5a5b76d68886e3dd6d55402dfd864f3fe10
Diego <diego.tanel@tecneg.com>
2011-12-26 21:45:43 -0300

    Exercises 5.5

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -26,4 +26,34 @@ describe "LayoutLinks" do
     get '/signup'
     response.should have_selector('title', :content => "Sign up")
   end
+
+  it "should have the right links on the layout - About" do
+    visit root_path
+    click_link "About"
+    response.should have_selector('title', :content => "About")
+  end
+
+  it "should have the right links on the layout - Help" do
+    visit root_path
+    click_link "Help"
+    response.should have_selector('title', :content => "Help")
+  end
+
+    it "should have the right links on the layout - Contact" do
+    visit root_path
+    click_link "Contact"
+    response.should have_selector('title', :content => "Contact")
+    end
+
+    it "should have the right links on the layout - Home" do
+    visit root_path
+    click_link "Home"
+    response.should have_selector('title', :content => "Home")
+    end
+
+    it "should have the right links on the layout - Sign up now!" do
+    visit root_path
+    click_link "Sign up now!"
+    response.should # fill in
+  end
 end
